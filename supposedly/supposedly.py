17be789129526afb14c27f7ae76bd5c06e703ea2
ftwtech <ftwtech@mailbox.org>
2018-08-07 19:33:08 +0000

    supposedly done

--- supposedly/supposedly.py
+++ supposedly/supposedly.py
@@ -9,7 +9,7 @@ class Supposedly:
 
     def __init__(self, bot):
         self.bot = bot
-        default_guild = {"supposedly":False}
+        default_guild = {"enabled":False, "frequency": 250}
         self.config = Config.get_conf(self, 18107945176)
         self.config.register_guild(**default_guild)
 
@@ -17,21 +17,33 @@ class Supposedly:
         guild = message.guild
         channel = message.channel
         author = message.author
-        randomInt = randint(0, 250)
+        max = await self.config.guild(guild).frequency()
+        randomInt = randint(0, max)
         if message.author.bot:
             return
         if randomInt == 1:
-            if await self.config.guild(guild).supposedly():
+            if await self.config.guild(guild).enabled():
                 await channel.send("*Supposedly...*")
 
     @commands.command(pass_context=True)
     @checks.mod_or_permissions(manage_channels=True)
-    async def supposedly(self, ctx):
+    async def supposedlytoggle(self, ctx):
         """on/off"""
         guild = ctx.message.guild
-        if not await self.config.guild(guild).supposedly():
-            await self.config.guild(guild).supposedly.set(True)
+        if not await self.config.guild(guild).enabled():
+            await self.config.guild(guild).enabled.set(True)
             await ctx.send("on")
         else:
-            await self.config.guild(guild).supposedly.set(False)
+            await self.config.guild(guild).enabled.set(False)
             await ctx.send("off")
+
+    @commands.command(pass_context=True)
+    @checks.mod_or_permissions(manage_channels=True)
+    async def supposedlyfreq(self, ctx, frequency:int=250):
+        """frequency"""
+        guild = ctx.message.guild
+        if not await self.config.guild(guild).enabled():
+            await ctx.send("I'm not setup on this guild!")
+            return
+        await self.config.guild(guild).frequency.set(frequency)
+        await ctx.send("Frequency set to {}.".format(frequency))
